e95392dbc987c96dfdb5039b18c77bf15de86904
Jkr200210 <kavishanrandi@gmail.com>
2025-12-18 11:22:04 +0530

    minimize the DOctor dashboard code

---
+++ src/pages/DoctorsPage/PatientOwerview.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { 
+  Heart, 
+  Bell, 
+  Settings, 
+  Activity,
+  AlertTriangle,
+  FileText,
+  Search,
+  User,
+  Clock,
+  Thermometer,
+  Droplets,
+  Eye,
+  LogOut,
+  Users,
+  UserCircle,
+  AlertCircle,
+  Tablet,
+  TrendingUp,
+  Wifi
+} from "lucide-react";
+
+function PatientOwerview(){
+    return(
+
+    );
+}
