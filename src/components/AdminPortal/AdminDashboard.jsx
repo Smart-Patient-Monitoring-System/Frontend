2045dab9325f56dda511c8f4ed695f1574031b00
Jkr200210 <kavishanrandi@gmail.com>
2025-12-20 05:48:30 +0530

    start the making admin portal

---
+++ src/components/AdminPortal/AdminDashboard.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { 
+  Heart, 
+  Bell, 
+  Settings, 
+  Activity,
+  AlertTriangle,
+  FileText,
+  Search,
+  User,
+  Clock,
+  Thermometer,
+  Droplets,
+  Eye,
+  LogOut,
+  Users,
+  UserCircle,
+  AlertCircle,
+  Tablet,
+  TrendingUp,
+  Wifi
+} from "lucide-react";
