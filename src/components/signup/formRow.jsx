c5ffe9d9178156cb45a875c3f8b9ce735136fd59
V3N1TH <152465117+VenithAbey@users.noreply.github.com>
2025-12-31 16:27:00 +0530

    Revert "Implement login and signup functionality for patient, doctor, nurse, and admin with form validation and error handling"
    This reverts commit 72c69177ccfb716295e57027a2fe22026000f786.

--- src/components/signup/formRow.jsx
+++ src/components/signup/formRow.jsx
@@ -1,26 +1,22 @@
-export default function FormRow({ label, name, type = "text", value, onChange, required, placeholder }) {
+export default function FormRow({ label }) {
     return (
         <div className="flex flex-col sm:flex-row items-start sm:items-center gap-2 w-full">
             {/* Label */}
             <span className="font-bold text-[16px] sm:text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px] w-full sm:w-[30%] md:w-[28%] lg:w-[26%] text-left">
-                {label} {required && <span className="text-red-500">*</span>}
+                {label}
             </span>
 
             {/* Input */}
             <input
-                type={type}
-                name={name}
-                value={value || ''}
-                onChange={onChange}
-                placeholder={placeholder}
-                required={required}
+                type="text"
                 className="
           w-full sm:w-[65%] md:w-[70%] lg:w-[78%] xl:w-[74%]
           h-10 md:h-11 lg:h-12
           bg-white 
           rounded-full px-4
           border border-[#D9D9D9]
-          outline-none focus:border-[#0090EE]
+          
+          
         "
             />
         </div>
