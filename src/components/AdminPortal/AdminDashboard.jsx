9353fe453b9c68694adcbd0f627ebf600feae0ec
Jkr200210 <kavishanrandi@gmail.com>
2026-01-01 21:06:57 +0530

    make the analytics page

--- src/components/AdminPortal/AdminDashboard.jsx
+++ src/components/AdminPortal/AdminDashboard.jsx
@@ -10,6 +10,7 @@ import {
 import admin from "../../assets/images/admin.png";
 import UserManagement from "../../pages/AdminPages/UserManagement";
 import IotDevices from "../../pages/AdminPages/IotDevices";
+import Analytics from "../../pages/AdminPages/Analytics";
 
 
 
