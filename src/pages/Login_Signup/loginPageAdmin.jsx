1a45b5864a9e5335b074610e4be64dfcea39a3d5
Jkr200210 <kavishanrandi@gmail.com>
2026-01-02 12:35:21 +0530

    finalize the home button in the admin login page

--- src/pages/Login_Signup/loginPageAdmin.jsx
+++ src/pages/Login_Signup/loginPageAdmin.jsx
@@ -10,76 +10,35 @@ export default function LoginPageAdmin() {
     return (
         <div className="w-full min-h-screen bg-[#F8FBFF] flex justify-center items-center p-4">
             <div className="w-full max-w-[1800px] grid grid-cols-1 lg:grid-cols-2 gap-10">
-
                 <div className="flex flex-col items-center lg:items-start gap-10 p-4">
-
                     <button
-                                className="
-                                    relative w-[140px] sm:w-[160px] md:w-[170px] lg:w-[150px] xl:w-[140px] h-[45px] sm:h-[50px] md:h-[48px] lg:h-[42px] xl:h-[40px]
-                                    rounded-full bg-gradient-to-r from-[#057EF8] to-[#0DC0BD] flex items-center px-3 sm:px-4 text-sm md:text-base
-                                    lg:text-sm font-semibold text-white hover:scale-105 transition-all duration-300 shadow-md hover:shadow-lg cursor-pointer"
-                                >
-                                {/* Left icon space */}
-                                <div className="w-[26px] sm:w-[28px] flex justify-center">
-                                    <img
-                                    src={gtMark}
-                                    alt="gt mark"
-                                    className="
-                                        w-[18px] h-[18px] sm:w-[20px] sm:h-[20px] lg:w-[16px] lg:h-[16px] object-contain"
-                                    />
-                                </div>
-                    
-                                {/* Center text */}
-                                <span className="flex-1 text-center font-inter font-medium">
-                                    HOME
-                                </span>
-                    
-                                {/* Right balance space */}
-                                <div className="w-[26px] sm:w-[28px]" />
-                                </button>
-
-                    <div className="w-[150px] h-[150px] sm:w-[215px] sm:h-[205px] rounded-3xl bg-gradient-to-tr from-[#0DC0BD] to-[#057EF8] shadow-[0_0_8px_8px_#0090EE40] flex justify-center items-center">
-                        <img src={heart} alt="heart" className="w-[120px] sm:w-[162px]" />
-                    </div>
-
-                    <div className="text-center lg:text-left">
-                        <p className="text-4xl sm:text-6xl font-extralight">WELCOME!</p>
-                        <p className="text-xl sm:text-3xl font-extralight mt-2">
-                            Sign in to access your dashboard
-                        </p>
-                    </div>
-
-                    <div className="w-full max-w-[520px] bg-[#F7F3FF] rounded-3xl shadow-xl p-6 relative">
-                        <div className="flex items-center gap-6">
-                            <div className="w-[85px] h-[85px] rounded-full bg-[#A538FF] shadow-[0_0_20px_rgba(0,0,0,0.25)] flex items-center justify-center">
-                                <img src={admin} alt="Admin" className="w-[45px] h-[45px]" />
-                            </div>
-                            <div>
-                                <p className="text-3xl font-medium text-center md:text-left">Admin</p>
-
-                                <p className="text-lg font-light ">
-                                    Access your health records and vitals
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <button onClick={() => navigate("/adminSignup")} className="w-full max-w-[520px] h-[50px] bg-[#F5F1F1] rounded-2xl shadow-md flex items-center justify-center
-                             scale-100 hover:scale-105 transition-transform duration-300 cursor-pointer p-0 border-none">
-                        <span className="text-xl font-semibold">SignUp</span>
-                    </button>
+                        onClick={() => navigate("/")}
+                        className="
+                          relative w-[140px] sm:w-[160px] md:w-[170px] lg:w-[150px] xl:w-[140px] h-[45px] sm:h-[50px] md:h-[48px] lg:h-[42px] xl:h-[40px]
+                          rounded-full bg-gradient-to-r from-[#057EF8] to-[#0DC0BD] flex items-center px-3 sm:px-4 text-sm md:text-base
+                          lg:text-sm font-semibold text-white hover:scale-105 transition-all duration-300 shadow-md hover:shadow-lg cursor-pointer"
+                    >
+                {/* Left icon space */}
+                        <div className="w-[26px] sm:w-[28px] flex justify-center">
+                          <img
+                            src={gtMark}
+                            alt="gt mark"
+                            className="
+                            w-[18px] h-[18px] sm:w-[20px] sm:h-[20px] lg:w-[16px] lg:h-[16px] object-contain"
+                         />
+                        </div>          
+                {/* Center text */}
+                        <span className="flex-1 text-center font-inter font-medium">
+                          HOME
+                       </span>    
+                {/* Right balance space */}
+                        <div className="w-[26px] sm:w-[28px]" />
+                  </button>
                 </div>
 
                 <hr
-                    className="
-    hidden
-    lg:block
-    absolute left-1/2 top-1/2 
-    w-[1102px] 
-    border-t border-black 
-    rotate-90 
-    -translate-x-1/2 -translate-y-1/2
-  "
+                    className="hidden lg:block absolute left-1/2 top-1/2  w-[1102px] 
+                            border-t border-black  rotate-90 -translate-x-1/2 -translate-y-1/2"
                 />
 
                 <div className="flex justify-center p-4">
