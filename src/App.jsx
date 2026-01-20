e71bc617a821d796e37c3cd19b36978f0777ce5c
Kavindi487 <kavindividusari@gmail.com>
2026-01-20 07:39:27 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,10 @@ import Features from "./pages/HomePage/components/Features";
 import FooterCTA from "./pages/HomePage/components/FooterCTA";
 
 import RoleSelectionPage from "./pages/Login_Signup/RoleSelectPage";
-import LoginDoctor from "./pages/Login_Signup/loginPageDoctor";
-import LoginPatient from "./pages/Login_Signup/loginPagePatient";
-import LoginAdmin from "./pages/Login_Signup/loginPageAdmin";
+
+import LoginDoctor from "./pages/Login_Signup/LoginPageDoctor";
+import LoginPatient from "./pages/Login_Signup/LoginPagePatient";
+import LoginAdmin from "./pages/Login_Signup/LoginPageAdmin";
 
 import SignupDoctor from "./pages/Login_Signup/SignupPageDoctor";
 import SignupPatient from "./pages/Login_Signup/SignupPagePatient";
