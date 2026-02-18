efdd62517abc5e464fabe1aef9ab1153be8cc85a
Kavindi487 <kavindividusari@gmail.com>
2026-02-19 02:12:07 +0530

    updated with docker

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 
-  base: '/Frontend/',
+  base: '/',
    define: {
     global: 'window',
   },
