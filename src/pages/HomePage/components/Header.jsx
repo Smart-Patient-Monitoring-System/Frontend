122ba65b808fcc50a0562b487200405f7b59f8f8
Jkr200210 <kavishanrandi@gmail.com>
2026-03-14 23:27:36 +0530

    before push

--- src/pages/HomePage/components/Header.jsx
+++ src/pages/HomePage/components/Header.jsx
@@ -1,6 +1,5 @@
 import { Heart } from "lucide-react";
 import { useNavigate } from "react-router-dom";
-import logo from "./assets/images/smartcare-logo.png";
 
 function Header() {
   const navigate = useNavigate();
@@ -8,12 +7,9 @@ function Header() {
   return (
     <header className="bg-gradient-to-r from-teal-600 to-cyan-600 px-4 sm:px-6 lg:px-8 py-4">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
+        
         <div className="flex items-center gap-2 sm:gap-3">
-          <img 
-            src={logo} 
-            alt="Logo" 
-            className="w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10 object-contain flex-shrink-0"
-          />
+          <Heart size={32} className="text-white" fill="white" />
           <div>
             <h1 className="text-white text-lg sm:text-xl lg:text-2xl font-bold">
               SmartCare Monitor
